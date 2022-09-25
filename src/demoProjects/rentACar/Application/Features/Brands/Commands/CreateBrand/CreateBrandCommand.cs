f2a371c19735df2ddd132c9ae01ff99d6b3ab842
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-09-25 22:50:03 +0300

    CreateBrandCommand ve MappingProfile

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository brandRepository;
+            private readonly IMapper mapper;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                this.brandRepository = brandRepository;
+                this.mapper = mapper;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand mappedBrand = mapper.Map<Brand>(request);
+                Brand createdBrand = await brandRepository.AddAsync(mappedBrand);
+                CreatedBrandDto createdBrandDto = mapper.Map<CreatedBrandDto>(createdBrand);
+                return createdBrandDto;
+            }
+        }
+    }
+}
