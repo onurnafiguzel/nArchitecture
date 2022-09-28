21ab44a442cac0589187b5ccaef321112360c6f3
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-09-28 23:57:56 +0300

    RequestValidationBeahvior açıldı, validation tetiklendi

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommand>
+    {
+        public CreateBrandCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim boş olamaz");
+            RuleFor(x => x.Name).MinimumLength(2);
+        }
+    }
+}
