af8012b1fbdc386cff51bcca4735815d46380330
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-10-11 23:45:04 +0300

    GetBrandById tüm işlemler

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetByIdBrand/GetByIdBrandQuery.cs
@@ -0,0 +1,36 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Queries.GetByIdBrand
+{
+    public class GetByIdBrandQuery : IRequest<BrandGetByIdDto>
+    {
+        public int Id { get; set; }
+
+        public class GetByIdBrandQueryHandler : IRequestHandler<GetByIdBrandQuery, BrandGetByIdDto>
+        {
+            private readonly IBrandRepository brandRepository;
+            private readonly IMapper mapper;
+            private readonly BrandBusinessRules brandBusinessRules;
+
+            public GetByIdBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+            {
+                this.brandRepository = brandRepository;
+                this.mapper = mapper;
+                this.brandBusinessRules = brandBusinessRules;
+            }
+
+            public async Task<BrandGetByIdDto> Handle(GetByIdBrandQuery request, CancellationToken cancellationToken)
+            {
+                Brand? brand = await brandRepository.GetAsync(b => b.Id == request.Id);
+                brandBusinessRules.BrandShouldExistWhenRequested(brand);
+                BrandGetByIdDto brandGetByIdDto = mapper.Map<BrandGetByIdDto>(brand);
+                return brandGetByIdDto;
+            }
+        }
+    }
+}
--- src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -19,5 +19,9 @@ namespace Application.Features.Brands.Rules
             IPaginate<Brand> result = await brandRepository.GetListAsync(b => b.Name == name);
             if (result.Items.Any()) throw new BusinessException("Brand name exists.");
         }
+        public void BrandShouldExistWhenRequested(Brand brand)
+        {
+            if (brand == null) throw new BusinessException("Requested brand does not exist");
+        }
     }
 }
