fdde91caa14056ccdf4e1d6415c0583d49d2a0a6
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-10-18 15:00:42 +0300

    ModelsController GetList metodu

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries.GetListModel;
+using Core.Application.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ModelsController : BaseController
+{
+    [HttpGet]
+    public async Task<ActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListModelQuery getListModelQuery = new GetListModelQuery { PageRequest = pageRequest };
+        ModelListModel result = await Mediator.Send(getListModelQuery);
+        return Ok(result);
+    }
+}
