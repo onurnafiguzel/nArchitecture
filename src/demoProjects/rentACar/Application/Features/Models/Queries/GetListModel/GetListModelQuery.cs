5935071e76a3f650e007d492480833662f5a32eb
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-10-18 13:18:51 +0300

    GetListModelQueryHandler oluşturuldu

---
+++ src/demoProjects/rentACar/Application/Features/Models/Queries/GetListModel/GetListModelQuery.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Models.Models;
+using Core.Application.Requests;
+using MediatR;
+
+namespace Application.Features.Models.Queries.GetListModel;
+
+public class GetListModelQuery : IRequest<ModelListModel>
+{
+    public PageRequest PageRequest { get; set; }
+
+    public class GetListModelQueryHandler : IRequestHandler<GetListModelQuery, ModelListModel>
+    {
+        public Task<ModelListModel> Handle(GetListModelQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
